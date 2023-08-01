c15d1d348b0aceb9fde6d7dd528fa7c7c4f4f8e0
Yogesh Shamlin Shinanth <yogeshshamlin@gmail.com>
2023-08-02 03:30:40 +0530

    adding JavaScript for interaction

---
+++ script.js
@@ -0,0 +1,9 @@
+const tabTitle = document.title;
+
+window.addEventListener("blur", ( )=> {
+    document.title = "Come Back, Grab an EV ! "
+})
+
+window.addEventListener("focus" , ()=> {
+    document.title = tabTitle;
+})
