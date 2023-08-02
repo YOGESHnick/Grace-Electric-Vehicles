8bb2aded14efa8888e45ac86579e7817f6fc4ca6
Yogesh Shamlin Shinanth <yogeshshamlin@gmail.com>
2023-08-02 22:46:19 +0530

    testing cursor events (mouse drag animation)

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Tab changes
 const tabTitle = document.title;
 
 window.addEventListener("blur", ( )=> {
@@ -7,3 +8,11 @@ window.addEventListener("blur", ( )=> {
 window.addEventListener("focus" , ()=> {
     document.title = tabTitle;
 })
+
+// Cursor function:
+var cursor = document.querySelector(".cursor");
+var cursor2 = document.querySelector(".cursor2");
+document.addEventListener("mousemove",function(e)
+    {
+      cursor.style.cssText=cursor2.style.cssText = "left: "+e.clientX + "px; top: "+e.clientY +"px;" ;
+    });
