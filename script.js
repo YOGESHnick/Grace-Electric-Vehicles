7e7b0ad20048bcecc40eb44a0421556aa786e48c
Yogesh Shamlin Shinanth <yogeshshamlin@gmail.com>
2023-08-03 09:20:42 +0530

    removing cursor animation features

--- script.js
+++ script.js
@@ -8,11 +8,3 @@ window.addEventListener("blur", ( )=> {
 window.addEventListener("focus" , ()=> {
     document.title = tabTitle;
 })
-
-// Cursor function:
-var cursor = document.querySelector(".cursor");
-var cursor2 = document.querySelector(".cursor2");
-document.addEventListener("mousemove",function(e)
-    {
-      cursor.style.cssText=cursor2.style.cssText = "left: "+e.clientX + "px; top: "+e.clientY +"px;" ;
-    });
